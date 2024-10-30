1e123c729fffdced48c09c06affde614b020a27c
johnnyfdev <johnnyfreirejh@hotmail.com>
2024-10-30 00:04:07 -0300

    Create Login Activity

---
+++ app/src/main/java/com/example/myapitest/LoginActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapitest
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myapitest.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setupGoogleLogin()
+      setupView()
+    }
+
+    private fun setupView() {
+        TODO("Not yet implemented")
+    }
+
+    private fun setupGoogleLogin() {
+        TODO("Not yet implemented")
+    }
+}

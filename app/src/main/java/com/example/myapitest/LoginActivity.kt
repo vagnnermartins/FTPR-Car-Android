0f26bf6cc1ae188f0466b822411745fa8c93dac2
johnnyfdev <johnnyfreirejh@hotmail.com>
2024-11-04 13:03:11 -0300

    feat: USe Google SignIn

--- app/src/main/java/com/example/myapitest/LoginActivity.kt
+++ app/src/main/java/com/example/myapitest/LoginActivity.kt
@@ -1,17 +1,29 @@
 package com.example.myapitest
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.myapitest.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
-
-
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInLauncher: ActivityResultLauncher<Intent>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,14 +38,59 @@ class LoginActivity : AppCompatActivity() {
         }
 
         setupGoogleLogin()
-      setupView()
+        setupView()
     }
 
     private fun setupView() {
-        TODO("Not yet implemented")
+        binding.loginSocialButton.setOnClickListener {
+            signIn()
+        }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        googleSignInLauncher.launch(signInIntent)
+
     }
 
     private fun setupGoogleLogin() {
-        TODO("Not yet implemented")
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("1090783472355-j3vu4lkd5esm6iqlr7jrn24e92289gmv.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = FirebaseAuth.getInstance()
+        googleSignInLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+                try {
+                    val account = task.getResult(ApiException::class.java)
+
+                    account.idToken?.let { idToken ->
+                        Log.d("LoginActivity", "firebaseAuthWithGoogle: $idToken")
+                        firebaseAuthWithGoogle(account?.idToken!!)
+                    }
+
+
+                } catch (e: ApiException) {
+                    Log.e("LoginActivity", "Google sign in failed", e)
+                }
+            }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credntial = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credntial)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = auth.currentUser
+                    Log.d("LoginActivity", "signInWithCredential:success $user")
+                } else {
+                    Log.w("LoginActivity", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/example/myapitest/MainActivity.kt
+++ app/src/main/java/com/example/myapitest/MainActivity.kt
@@ -3,10 +3,15 @@ package com.example.myapitest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapitest.databinding.ActivityMainBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +19,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         requestLocationPermission()
         setupView()
+
 
         // 1- Criar tela de Login com algum provedor do Firebase (Telefone, Google)
         //      Cadastrar o Seguinte celular para login de test: +5511912345678
@@ -30,13 +36,16 @@ class MainActivity : AppCompatActivity() {
         // Opcionalmente trabalhar com o Google Maps ara enviar o place
     }
 
+
     override fun onResume() {
         super.onResume()
         fetchItems()
     }
 
+
+
     private fun setupView() {
-        // TODO
+
     }
 
     private fun requestLocationPermission() {

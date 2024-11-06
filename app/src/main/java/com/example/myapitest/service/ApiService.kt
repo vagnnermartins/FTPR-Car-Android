92505001ab153ab3e3baca0b0b97cc225d772716
johnnyfdev <johnnyfreirejh@hotmail.com>
2024-11-06 20:43:21 -0300

    feat: create delete car page

--- app/src/main/java/com/example/myapitest/service/ApiService.kt
+++ app/src/main/java/com/example/myapitest/service/ApiService.kt
@@ -3,6 +3,7 @@ package com.example.myapitest.service
 
 import com.example.myapitest.models.Car
 import com.example.myapitest.models.ResponseCarWrapper
+import retrofit2.http.DELETE
 
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -15,4 +16,6 @@ interface ApiService {
     @GET("car/{id}")
     suspend fun getCar(@Path("id") id: String): ResponseCarWrapper
 
+    @DELETE("car/{id}")
+    suspend fun deleteCar(@Path("id") id: String)
 }
